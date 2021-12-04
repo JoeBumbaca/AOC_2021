f1c286537d40d92e47d39271a28ed741c315b97d
joe bumbaca <joebumbaca@gmail.com>
2021-12-04 00:10:36 -0800

    Finish part 1

--- day_01/output1.js
+++ day_01/output1.js
@@ -2,3 +2,24 @@ const fs = require('fs');
 const text = fs.readFileSync('./input1.txt', 'utf-8');
 let input = text.split('\n').map(el => { return parseInt(el) });
 
+// Part 1
+
+const largerThanPrev = function(input) {
+  
+  let count = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    let current = input[i];
+    let next = input[i + 1];
+    if (next > current) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+// console.log(largerThanPrev(input));
+
+// Part 2
+
