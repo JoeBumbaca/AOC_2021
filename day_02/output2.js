0cd6e449fa688ebd689175a6330506295caa1851
joe bumbaca <joebumbaca@gmail.com>
2021-12-04 14:43:13 -0800

    Finish d2 p2

--- day_02/output2.js
+++ day_02/output2.js
@@ -3,7 +3,7 @@ const text = fs.readFileSync('./input2.txt', 'utf-8');
 let input = text.split('\n').map((data) => {
   let arr = [];
   let parts = data.split(" ");
-  arr.push(parts[0], parts[1])
+  arr.push(parts[0], parseInt(parts[1]))
   return arr;
 })
 
@@ -18,13 +18,13 @@ const finalDepth = (data) => {
 
     switch(datum[0]){
       case 'forward':
-        pos[0] += parseInt(datum[1])
+        pos[0] += datum[1];
         break;
       case 'down':
-        pos[1] += parseInt(datum[1])
+        pos[1] += datum[1];
         break;
       case 'up':
-        pos[1] -= parseInt(datum[1])
+        pos[1] -= datum[1];
         break;
       default:
         break;
@@ -34,4 +34,32 @@ const finalDepth = (data) => {
   return pos[0] * pos[1]
 }
 
-console.log(finalDepth(input))
+// console.log(finalDepth(input))
+
+// Part 2
+
+const finalDepth2 = (data) => {
+  let pos = [0,0,0];
+
+  data.forEach(datum => {
+
+    switch(datum[0]){
+      case 'forward':
+        pos[0] += datum[1];
+        pos[1] += (pos[2] * datum[1])
+        break;
+      case 'down':
+        pos[2] += datum[1];
+        break;
+      case 'up':
+        pos[2] -= datum[1];
+        break;
+      default:
+        break;
+    }
+  })
+
+  return pos[0] * pos[1]
+}
+
+// console.log(finalDepth2(input));
