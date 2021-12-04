07f5dea6e3ecf635a299f761d3438c4767a32220
joe bumbaca <joebumbaca@gmail.com>
2021-12-04 00:44:33 -0800

    Finish part 2

--- day_01/output1.js
+++ day_01/output1.js
@@ -4,13 +4,13 @@ let input = text.split('\n').map(el => { return parseInt(el) });
 
 // Part 1
 
-const largerThanPrev = function(input) {
+const largerThanPrev = function(data) {
   
   let count = 0;
 
-  for (let i = 0; i < input.length; i++) {
-    let current = input[i];
-    let next = input[i + 1];
+  for (let i = 0; i < data.length; i++) {
+    let current = data[i];
+    let next = data[i + 1];
     if (next > current) {
       count += 1;
     }
@@ -22,4 +22,32 @@ const largerThanPrev = function(input) {
 // console.log(largerThanPrev(input));
 
 // Part 2
+// const arr = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+const add = (prev, curr) => prev + curr;
 
+const threeSumIncrease = function(data) {
+
+  let count = 0;
+  let currSum;
+  for (let i = 0; i < data.length; i++) {
+    if (i + 3 > data.length) {
+      break;
+    } else {
+      let window = data.slice(i, i+3);
+      if (currSum) {
+        let nextSum = window.reduce(add, 0);
+        if (nextSum > currSum) {
+          count += 1;
+          currSum = nextSum;
+        } else {
+          currSum = nextSum;
+        }
+      } else {
+        currSum = window.reduce(add, 0);
+      }
+    }
+  }
+  return count;
+};
+
+console.log(threeSumIncrease(input));
